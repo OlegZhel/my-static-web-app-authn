b1362c4284edefc8b6c696944a1e1854a04c3dd8
Oleg Zheldakov <ozheldakov@gmail.com>
2024-02-26 16:01:21 +0200

    auth enable

--- angular-app/src/app/core/components/nav.component.ts
+++ angular-app/src/app/core/components/nav.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInfo } from '../model/user-info';
 
 @Component({
   selector: 'app-nav',
   template: `
+  <nav class="menu auth">
+  <p class="menu-label">Auth</p>
+  <div class="menu-list auth">
+    <ng-container *ngIf="!userInfo; else logout">
+      <ng-container *ngFor="let provider of providers">
+        <a href="/.auth/login/{{provider}}?post_login_redirect_uri={{redirect}}">{{provider}}</a>
+      </ng-container>
+    </ng-container>
+    <ng-template #logout>
+      <a href="/.auth/logout?post_logout_redirect_uri={{redirect}}">Logout</a>
+    </ng-template>
+  </div>
+  </nav>
     <nav class="menu">
       <p class="menu-label">Menu</p>
       <ul class="menu-list">
@@ -14,6 +28,34 @@ import { Component } from '@angular/core';
         </a>
       </ul>
     </nav>
+    <div class="user" *ngIf="userInfo">
+  <p>Welcome</p>
+  <p>{{ userInfo?.userDetails }}</p>
+  <p>{{ userInfo?.identityProvider }}</p>
+</div>
   `,
 })
-export class NavComponent {}
+export class NavComponent implements OnInit {
+  public userInfo: UserInfo;
+  providers = ['twitter', 'github', 'aad'];
+  redirect = window.location.pathname;
+
+  async ngOnInit() {
+    this.userInfo = await this.getUserInfo();
+  }
+
+  async getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+
+      console.log('Principal:');
+      console.log(clientPrincipal);
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
+}
